3b17f7bcae5a7f9fb3aad3d6917b1bb4883b4737
Rafael Albino <fael10pg@gmail.com>
2021-11-26 22:11:40 -0300

    Exercicio 6 - usando LocalDate

--- src/Modulo1/M1S04/calculaidade/CalculaIdade.java
+++ src/Modulo1/M1S04/calculaidade/CalculaIdade.java
@@ -2,40 +2,40 @@ package calculaidade;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.format.ResolverStyle;
 import java.util.Date;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class CalculaIdade {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 
 		Scanner sc = new Scanner(System.in);
 
 		System.out.print("Digite sua data de aniversário(Ex: 01/01/2000): ");
 		String dataNascimento = sc.next();
+		LocalDate aniversario = null;
 
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		Date dataAtual = new Date();
-		Date aniversario = null;
-
+		boolean ehDataValida;
 		try {
-			aniversario = sdf.parse(dataNascimento);
-		} catch (ParseException e) {
-			e.printStackTrace();
+			DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy").withResolverStyle(ResolverStyle.STRICT);
+			aniversario = LocalDate.parse(dataNascimento, dtf);
+			ehDataValida = true;
+		} catch (DateTimeParseException e) {
+			ehDataValida = false;
 		}
-
-		long diferenca = dataAtual.getTime() - aniversario.getTime();
-
-		TimeUnit time = TimeUnit.DAYS;
-		long idade = time.convert(diferenca, TimeUnit.MILLISECONDS) / 365;
-
-		if (idade < 18) {
-			System.out.print("Você tem " + idade + " anos. Você é menor de idade!!");
-		} else {
-			System.out.print("Você tem " + idade + " anos. Você é maior de idade!!");
+		if (ehDataValida) {
+			int idade = Period.between(aniversario, LocalDate.now()).getYears();
+			System.out.print(idade < 18 ? "\nVocê tem " + idade + " anos. Você é menor de idade!!" :
+					"\nVocê tem " + idade + " anos. Você é maior de idade!!");
+		}else{
+			System.out.println("Digite uma data válida!!");
 		}
-
 	}
-
 }
