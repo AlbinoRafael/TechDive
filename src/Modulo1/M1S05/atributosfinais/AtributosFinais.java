b63139b6676f45a14ee51b7e0712360cac696f8a
Rafael Albino <fael10pg@gmail.com>
2021-12-03 13:51:18 -0300

    M1S05 - Exercicio 4

---
+++ src/Modulo1/M1S05/atributosfinais/AtributosFinais.java
@@ -0,0 +1,11 @@
+package M1S05.atributosfinais;
+
+public class AtributosFinais {
+
+    private final int _FINAL_ATTRIBUTE1_;
+   private final static int _FINAL_ATTRIBUTE2_ = 2;
+
+   public AtributosFinais(){
+       _FINAL_ATTRIBUTE1_ = 1;
+   }
+}
