16728a5cc0406c6664b68dc00ade062830a3fba5
Rafael Albino <fael10pg@gmail.com>
2021-12-03 14:52:04 -0300

    M1S05 - Exercicio 5

---
+++ src/Modulo1/M1S05/classe2/Classe2.java
@@ -0,0 +1,16 @@
+package M1S05.classe2;
+
+public class Classe2 {
+
+    public Classe2(){
+        super();
+    }
+
+    public void mensagem(){
+        System.out.println("metodo da classe 2");
+    }
+
+    public static void mensagemStatic(){
+        System.out.println("Metodo estatico da classe 2");
+    }
+}
