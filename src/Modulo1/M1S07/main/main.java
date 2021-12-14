6fcd7156138ba8bd0873de0bf0601a34d5e74d5b
Rafael Albino <fael10pg@gmail.com>
2021-12-14 16:02:35 -0300

    m1s07-ex6 - Criando classe Vendedor e testando

--- src/Modulo1/M1S07/main/main.java
+++ src/Modulo1/M1S07/main/main.java
@@ -1,23 +1,20 @@
 package main;
 
-import pessoa.Administrador;
-import pessoa.Empregado;
-import pessoa.Fornecedor;
-import pessoa.Operario;
+import pessoa.*;
 
 public class main {
 
     public static void main(String[] args) {
-        Operario opr = new Operario("Marcos", "Rua rua", "88888-8888", 3,4000,15,1200,15);
+        Vendedor vnd = new Vendedor("Marcos", "Rua rua", "88888-8888", 1,1700,10,2200,15);
 
-        System.out.println("Setor: "+opr.getCodigoSetor());
-        System.out.println("Nome: "+opr.getNome());
-        System.out.println("Endereco: "+opr.getEndereco());
-        System.out.println("Telefone: "+opr.getTelefone());
-        System.out.println("Salario bruto: R$ "+opr.getSalarioBase());
-        System.out.println("valor do imposto: "+opr.getImposto()+"%");
-        System.out.println("Valor da produção: R$ "+opr.getValorProducao());
-        System.out.println("Comissão: "+opr.getComissao()+"%");
-        System.out.println("Salario limpo: R$ "+opr.calcularSalario());
+        System.out.println("Setor: "+vnd.getCodigoSetor());
+        System.out.println("Nome: "+vnd.getNome());
+        System.out.println("Endereco: "+vnd.getEndereco());
+        System.out.println("Telefone: "+vnd.getTelefone());
+        System.out.println("Salario bruto: R$ "+vnd.getSalarioBase());
+        System.out.println("valor do imposto: "+vnd.getImposto()+"%");
+        System.out.println("Valor das vendas: R$ "+vnd.getValorVendas());
+        System.out.println("Comissão: "+vnd.getComissao()+"%");
+        System.out.println("Salario limpo: R$ "+vnd.calcularSalario());
     }
 }
