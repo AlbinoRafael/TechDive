08ca1dcf5e7ec4ad7afc9d3a791871a842b4b8a5
Rafael Albino <fael10pg@gmail.com>
2022-02-28 14:24:23 -0300

    m1s09-ex7 - excessões para garantir agendar com antecedência

--- src/hotel/agenda/Reserva.java
+++ src/hotel/agenda/Reserva.java
@@ -10,85 +10,81 @@ import java.time.format.DateTimeFormatter;
 
 public class Reserva {
 
-   private LocalDate dataAgendamento;
-   private LocalDate dataFimAgendamento;
-   private Hospede hospede;
-   private Quarto quarto;
+    private LocalDate dataAgendamento;
+    private LocalDate dataFimAgendamento;
+    private Hospede hospede;
+    private Quarto quarto;
 
-   public Reserva(Hospede hospede, Quarto quarto) {
-      this.hospede = hospede;
-      this.quarto = quarto;
-   }
+    public Reserva(Hospede hospede, Quarto quarto) {
+        this.hospede = hospede;
+        this.quarto = quarto;
+    }
 
-   public LocalDate getDataAgendamento() {
-      return dataAgendamento;
-   }
+    public LocalDate getDataAgendamento() {
+        return dataAgendamento;
+    }
 
-   public void setDataAgendamento(LocalDate dataAgendamento) {
-      this.dataAgendamento = dataAgendamento;
-   }
+    public LocalDate getDataFimAgendamento() {
+        return dataFimAgendamento;
+    }
 
-   public LocalDate getDataFimAgendamento(){
-      return dataFimAgendamento;
-   }
+    public Hospede getHospede() {
+        return hospede;
+    }
 
-   public Hospede getHospede() {
-      return hospede;
-   }
+    public void setHospede(Hospede hospede) {
+        this.hospede = hospede;
+    }
 
-   public void setHospede(Hospede hospede) {
-      this.hospede = hospede;
-   }
+    public Quarto getQuarto() {
+        return quarto;
+    }
 
-   public Quarto getQuarto() {
-      return quarto;
-   }
+    public void setQuarto(Quarto quarto) {
+        this.quarto = quarto;
+    }
 
-   public void setQuarto(Quarto quarto) {
-      this.quarto = quarto;
-   }
-
-   public void agendarReserva(String dataAgendamento, int diasReserva){
-      LocalDate dataAgenda = LocalDate.parse(dataAgendamento);
-      if(dataAgenda.isBefore(LocalDate.now().plusDays(60))) {
-         if (quarto.estaDisponivel(dataAgenda,diasReserva)) {
-            this.dataAgendamento = dataAgenda;
-            this.dataFimAgendamento = dataAgenda.plusDays(diasReserva);
-         }else{
-            System.err.println("O quarto não está disponível para reserva!");
-         }
-      }else{
-         System.err.println("Você precisa agendar com pelo menos 60 dias de antecedência!");
-      }
-   }
-
-   public static void registraReserva(Reserva reserva) {
-      DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-      LocalDate inicioAltaTemporada = LocalDate.parse("01-10-" + LocalDate.now().getYear(), dtf);
-      LocalDate fimAltaTemporada = LocalDate.parse("31-03-" + LocalDate.now().getYear() + 1, dtf);
-      for (Quarto quarto : Hotel.getQuartos()) {
-         if (quarto.getNumeroQuarto() == reserva.getQuarto().getNumeroQuarto()) {
-            if (reserva.getDataAgendamento().isAfter(fimAltaTemporada) && reserva.getDataAgendamento().isBefore(inicioAltaTemporada)) {
-               if (reserva.getQuarto().getTipoQuarto().equals(EnumTipoQuarto.SIMPLES)) {
-                  reserva.getQuarto().setValorDiaria(140);
-               } else if (reserva.getQuarto().getTipoQuarto().equals(EnumTipoQuarto.LUXO)) {
-                  reserva.getQuarto().setValorDiaria(400);
-               }
+    public void agendarReserva(String dataAgendamento, int diasReserva) {
+        LocalDate dataAgenda = LocalDate.parse(dataAgendamento);
+        if (dataAgenda.isBefore(LocalDate.now().plusDays(60))) {
+            if (quarto.estaDisponivel(dataAgenda, diasReserva)) {
+                this.dataAgendamento = dataAgenda;
+                this.dataFimAgendamento = dataAgenda.plusDays(diasReserva);
             } else {
-               if (reserva.getQuarto().getTipoQuarto().equals(EnumTipoQuarto.SIMPLES)) {
-                  reserva.getQuarto().setValorDiaria(90);
-               } else if (reserva.getQuarto().getTipoQuarto().equals(EnumTipoQuarto.LUXO)) {
-                  reserva.getQuarto().setValorDiaria(150);
-               }
+                throw new IllegalArgumentException("O quarto não está disponível para reserva!");
             }
-            Hotel.getReservas().add(reserva);
-         }
-      }
-   }
+        } else {
+            throw new IllegalArgumentException("Você precisa agendar com pelo menos 60 dias de antecedência!");
+        }
+    }
 
-   @Override
-   public String toString() {
-      return "hospede " + hospede.getNome() +" "+ hospede.getSobrenome()+
-              ", quarto " + quarto.getNumeroQuarto();
-   }
+    public static void registraReserva(Reserva reserva) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        LocalDate inicioAltaTemporada = LocalDate.parse("01-10-" + LocalDate.now().getYear(), dtf);
+        LocalDate fimAltaTemporada = LocalDate.parse("31-03-" + LocalDate.now().getYear() + 1, dtf);
+        for (Quarto quarto : Hotel.getQuartos()) {
+            if (quarto.getNumeroQuarto() == reserva.getQuarto().getNumeroQuarto()) {
+                if (reserva.getDataAgendamento().isAfter(fimAltaTemporada) && reserva.getDataAgendamento().isBefore(inicioAltaTemporada)) {
+                    if (reserva.getQuarto().getTipoQuarto().equals(EnumTipoQuarto.SIMPLES)) {
+                        reserva.getQuarto().setValorDiaria(140);
+                    } else if (reserva.getQuarto().getTipoQuarto().equals(EnumTipoQuarto.LUXO)) {
+                        reserva.getQuarto().setValorDiaria(400);
+                    }
+                } else {
+                    if (reserva.getQuarto().getTipoQuarto().equals(EnumTipoQuarto.SIMPLES)) {
+                        reserva.getQuarto().setValorDiaria(90);
+                    } else if (reserva.getQuarto().getTipoQuarto().equals(EnumTipoQuarto.LUXO)) {
+                        reserva.getQuarto().setValorDiaria(150);
+                    }
+                }
+                Hotel.getReservas().add(reserva);
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "hospede " + hospede.getNome() + " " + hospede.getSobrenome() +
+                ", quarto " + quarto.getNumeroQuarto();
+    }
 }
