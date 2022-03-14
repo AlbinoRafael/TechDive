5873749f6d43ac34fc831e012f088c3911f88c52
Rafael Albino <fael10pg@gmail.com>
2022-03-14 13:50:35 -0300

    m2s03-ex2

--- src/market/connection/FactoryConnection.java
+++ src/market/connection/FactoryConnection.java
@@ -10,6 +10,7 @@ public class FactoryConnection {
 	
 	public Connection openConnection() throws SQLException {
 		System.out.println("Preparando para abrir conexão com o banco de dados!");
+		System.out.print("Digite o nome do usuario: ");
 		return DriverManager.getConnection("jdbc:postgresql://localhost/mercado?useTimezone=true&serverTimezone=UTC", 
 				"postgres", "1234");
 	}
